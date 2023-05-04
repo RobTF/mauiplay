7c4740d9ced741337d40908850079081a4a08bdc
Rob Harwood <robharwood@vismo.com>
2023-05-04 16:45:21 +0100

    added image layout issue

---
+++ ImageGridIssue/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace ImageGridIssue;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void LeftClicked(object sender, EventArgs e)
+    {
+		Console.WriteLine("LEFT BUTTON TAPPED!!");
+    }
+
+    private void RightClicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
